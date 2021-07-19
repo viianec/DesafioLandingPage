5b374c0f915db69ae50f345fc07f5570f96e8567
Viviane Cipriano <vivianecipriano.a@gmail.com>
2021-07-18 22:54:09 -0300

    landingPage

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome =  document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData);
+
+    let content = document.getElementById('content');
+
+    let carregando = '<p>Aguarde um momento...</p>'
+
+    content.innerHTML = carregando
+    setTimeout(()=>{
+        content.innerHTML = pronto
+    },2000)
+
+})
